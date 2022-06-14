65b0c3eb3b4244cfefa2c6820d46a76b8ba6d917
Tzuzul <mail@tzuzulcode.com>
2022-06-14 09:21:27 -0500

    Payment

---
+++ frontend/payment.js
@@ -0,0 +1,41 @@
+const stripe = Stripe("pk_test_51KTd1dCxJ8HWxsAUvHdkJU90wXuUHO4qa4bF5dq3A7kCPWLAiaPnQ4bDpvBqIVMHPdABDwVMODmDff6jl8ok59OJ00SeHORvaW");
+
+const form = document.getElementById("payment-form")
+const loadPaymentBtn = document.getElementById("loadPayment")
+
+
+loadPaymentBtn.onclick = ()=>{
+    initialize()
+}
+
+let elements;
+
+const initialize = async ()=>{
+    const response = await fetch("http://localhost:4000/api/cart/pay",{
+        credentials:"include"
+    })
+
+    const data = response.json()
+
+    const clientSecret = data.clientSecret
+
+    const appearance = {
+        theme: 'stripe',
+    };
+
+    elements = stripe.elements({ appearance, clientSecret });
+
+    const paymentElement = elements.create("payment");
+    paymentElement.mount("#payment-element");
+}
+
+form.onsubmit= async (event)=>{
+    event.preventDefault()
+
+    const result = await stripe.confirmPayment({
+        elements,
+
+    })
+
+    console.log(result)
+}
