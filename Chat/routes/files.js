c4970f34626856c56b14f18877938cd479c1c6e2
Tzuzul <mail@tzuzulcode.com>
2022-07-08 09:22:24 -0500

    Streams

--- Chat/routes/files.js
+++ Chat/routes/files.js
@@ -1,6 +1,7 @@
 const {Router} = require("express")
 const FilesService = require("../services/files")
 const upload = require("../middleware/upload")
+const busboy = require("busboy")
 
 function files(app){
     const router = Router()
@@ -8,13 +9,52 @@ function files(app){
 
     const filesServ = new FilesService()
 
-    router.post("/upload",upload.single("file"),async (req,res)=>{
-        const file = req.file.buffer
-        const fileName = req.file.originalname
+    router.get("/:fileName",async (req,res)=>{
+        const {fileName} = req.params
 
-        const result = await filesServ.upload(fileName,file)
+        const result = await filesServ.download(fileName)
 
-        return res.json(result)
+        if(result.success){
+            // readableStream.pipe(req)
+            return res.end(result.data.Body)
+        }
+        
+        // reto: descargar archivo mediante streams
+
+        return res.status(400).json(result)
+    })
+    router.post("/",async (req,res)=>{
+        let promise
+        const bb = busboy({ headers: req.headers });
+        bb.on('file', (name, file, info) => {
+            const { filename, encoding, mimeType } = info;
+            promise = filesServ.upload(filename,file)
+        })
+
+        bb.on('close', async () => {
+            console.log('Done parsing form!');
+            // res.writeHead(303, { Connection: 'close', Location: '/' });
+            const result = await promise
+            res.json(result)
+        });
+
+        // req: Readable stream
+        req.pipe(bb)
+        // const result = await filesServ.upload(fileName,file)
+
+        // return res.json(result)
+    })
+    router.delete("/:fileName",async (req,res)=>{
+        const {fileName} = req.params
+        console.log(fileName)
+
+        const result = await filesServ.delete(fileName)
+
+        if(result.success){
+            return res.json(result)
+        }
+
+        return res.status(400).json(result)
     })
 }
 
--- Chat/services/files.js
+++ Chat/services/files.js
@@ -1,21 +1,50 @@
 const AWS = require("aws-sdk")
 const { awsBucketName } = require("../config")
+const uuid = require("uuid")
+const path = require("path")
+const {PassThrough} = require("stream")
 
 const s3 = new AWS.S3()
 
 class Files{
-    async download(){
+    async download(fileName){
         // Reto
         // s3.getObject()
+        try {
+            const result = await s3.getObject({
+                Key:fileName,
+                Bucket:awsBucketName
+            }).promise()
+            
+
+            return {
+                success:true,
+                message:"File downloaded successfully",
+                data:result
+            }
+
+        } catch (error) {
+            console.log(error)
+            return {success:false,message:"An error ocurred"}
+        }
     }
 
     async upload(fileName,file){
         try {
+            console.log(file)
+            const ext = path.extname(fileName)
+            const Key = uuid.v4()+ext
+            // const passStream = new PassThrough()
+
             const result = await s3.upload({
                 Bucket:awsBucketName,
-                Key:fileName,
+                Key,
                 Body:file //Buffer
             }).promise()
+
+            console.log(result)
+
+            // Registrar en la BD el archivo
     
             return {
                 success:true,
@@ -29,10 +58,26 @@ class Files{
         }
     }
 
-    async delete(){
-        // 
-
-        // s3.deleteObject()
+    async delete(fileName){
+        try {
+            const result = await s3.deleteObject({
+                Key:fileName,
+                Bucket:awsBucketName
+            }).promise()
+    
+            return {
+                success:true,
+                message:"File deleted successfully",
+                key:fileName
+            }
+        } catch (error) {
+            console.log(error)
+            return {
+                success:false,
+                message:"An error ocurred",
+                key:fileName
+            }
+        }
     }
 }
 
